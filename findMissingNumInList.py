3cac21aa71b352b805e7e3b589eace65b9857c1b
py2k5 <pradipy2k5@gmail.com>
2017-08-18 11:22:27 +0530

    Create findMissingNumInList.py

---
+++ findMissingNumInList.py
@@ -0,0 +1,9 @@
+##This program finds a number missing in the list of 0 to N
+##Example [1,4,2,0]
+##output: 3
+def sumMethod(inputList):
+        # find (n*n+1)/2
+        fullSum = (len(inputList) * (len(inputList) + 1)) / 2 
+        # subtract sum of full range from supplied range
+        return fullSum - sum(inputList)
+print(sumMethod([1,2,3])) 
