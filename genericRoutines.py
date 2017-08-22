0cbcee45e60f10343898071ee31ad2853519db5a
py2k5 <pradipy2k5@gmail.com>
2017-08-22 17:48:09 +0530

    Update genericRoutines.py

--- genericRoutines.py
+++ genericRoutines.py
@@ -15,4 +15,15 @@ def fibonacci_by_generator(num):
        print(item)
 """
 
-    
+def countOccurancesInList(inp_list):
+    s=set()
+    for x in inp_list:
+        if x in s:
+            pass
+        else:
+    	    print(x,inp_list.count(x))
+    	    s.add(x)
+
+"""test the function
+countOccurancesInList([1,2,3,4,3,2,3,5,6,2,3,5,2,4,6,32,5,1,43,32,2,3,4,21,1,2,4,45,32,1,3,0])
+"""
