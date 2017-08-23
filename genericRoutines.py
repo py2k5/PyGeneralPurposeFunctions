fcdbfbe9bf55173c79dd7f6ac3352e59872c2b88
py2k5 <pradipy2k5@gmail.com>
2017-08-23 18:17:54 +0530

    Update genericRoutines.py

--- genericRoutines.py
+++ genericRoutines.py
@@ -1,19 +1,23 @@
+#####################################################################################################################
 """Find the uniqe elements in a list of elements"""
 def uniqueList(inp_list):
     uniq_list = list(set(inp_list))
     return uniq_list
 
+#####################################################################################################################
 """Generate a fibonacci series using a generator"""
 def fibonacci_by_generator(num):
     a,b = 0,1
     for i in xrange(0,num):
         yield "{}:{}".format(i+1,a)
         a,b = b, a+b
+        
 """
 ###calling the fibonacci function
    for item in fibonacci_by_generator(10):
        print(item)
 """
+#####################################################################################################################
 
 """print the number and its count in a list in order of their appearances
 example : [ 1,2,3,3,2,3,4,2,1] => 1:2,2:3,3:3,4:1 
@@ -30,3 +34,15 @@ def countOccurancesInList(inp_list):
 """test the function
 countOccurancesInList([1,2,3,4,3,2,3,5,6,2,3,5,2,4,6,32,5,1,43,32,2,3,4,21,1,2,4,45,32,1,3,0])
 """
+#####################################################################################################################
+"""Transpose a multidimensional list 
+   for example -> [ [1,2,3],[4,5,6],[7,8,9] ]
+   should be transposed to [ [1,4,7],[2,5,8],[3,6,9] ]
+"""
+def transpose(l):
+    #method1
+    res1 = [ [row[i] for row in l] for i in range(len(l)) ]
+    
+    #method2
+    res2 = [ list(i) for i in zip(*l) ]
+#####################################################################################################################    
