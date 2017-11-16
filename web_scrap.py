482a53592418213daba7d684ef6d5a6db46befc2
py2k5 <pradipy2k5@gmail.com>
2017-11-16 20:36:10 +0530

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,1 +1,13 @@
 
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
+url='http://www.facebook.com'
+
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc,'lxml')
+
+tags = soup('a')
+for tag in tags:
+  print(tag.get('href',None))
