54a9a80eb3cd6b3d24d43eb6ca8b6806ac44cf95
py2k5 <pradipy2k5@gmail.com>
2017-12-05 16:34:48 +0530

    Update find_common_elements_in_list.py

--- find_common_elements_in_list.py
+++ find_common_elements_in_list.py
@@ -1,4 +1,5 @@
 # Implement your function below.
+#Big O(n)
 def common_elements(list1, list2):
     p1 = 0
     p2 = 0
@@ -15,15 +16,20 @@ def common_elements(list1, list2):
     return result
 
 
+
 # NOTE: The following input values will be used for testing your solution.
 list_a1 = [1, 3, 4, 6, 7, 9]
 list_a2 = [1, 2, 4, 5, 9, 10]
+print(common_elements(list_a1,list_a2))
+
 # common_elements(list_a1, list_a2) should return [1, 4, 9] (a list).
 
 list_b1 = [1, 2, 9, 10, 11, 12]
 list_b2 = [0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15]
+print(common_elements(list_b1,list_b2))
 # common_elements(list_b1, list_b2) should return [1, 2, 9, 10, 12] (a list).
 
 list_c1 = [0, 1, 2, 3, 4, 5]
 list_c2 = [6, 7, 8, 9, 10, 11]
+print(common_elements(list_c1,list_c2))
 # common_elements(list_b1, list_b2) should return [] (an empty list).
