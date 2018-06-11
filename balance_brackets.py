459d75560f9883edeaf98cdc892da2540b96eecb
py2k5 <pradipy2k5@gmail.com>
2018-06-11 14:44:04 +0530

    Update balance_brackets.py

--- balance_brackets.py
+++ balance_brackets.py
@@ -16,3 +16,16 @@ def isValid(s):
     
     
 print(isValid('{[][[]][]()[]'))
+
+
+#######################Shorter version 
+BRACKETS_MAP = {"[": "]", "{": "}", "(": ")"}
+
+def isValid(s):
+    stack = []
+    for bracket in s:
+        if bracket in BRACKETS_MAP:
+            stack.append(BRACKETS_MAP[bracket])
+        elif not stack or bracket != stack.pop():
+            return False
+    return not stack
