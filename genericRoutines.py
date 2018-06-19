e073dac645404f062d0f43f9bd536d74679aa805
py2k5 <pradipy2k5@gmail.com>
2018-06-19 22:48:13 +0530

    Update genericRoutines.py

--- genericRoutines.py
+++ genericRoutines.py
@@ -28,11 +28,14 @@ def countOccurancesInList(inp_list):
         if x in s:
             pass
         else:
-    	    print(x,inp_list.count(x))
+    	    print("{}:{}".format(x,inp_list.count(x)), end=",")
     	    s.add(x)
 
 """test the function
 countOccurancesInList([1,2,3,4,3,2,3,5,6,2,3,5,2,4,6,32,5,1,43,32,2,3,4,21,1,2,4,45,32,1,3,0])
+output:
+1:4,2:6,3:6,4:4,5:3,6:2,32:3,43:1,21:1,45:1,0:1
+
 """
 
 ############### OR ###############
