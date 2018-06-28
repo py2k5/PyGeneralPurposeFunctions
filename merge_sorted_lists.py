2c6cfa52672c6c669f3f86c1c76d4321ea0b2316
py2k5 <pradipy2k5@gmail.com>
2018-06-28 16:26:04 +0530

    Update merge_sorted_lists.py

--- merge_sorted_lists.py
+++ merge_sorted_lists.py
@@ -25,3 +25,32 @@ def merge_lists(l1,l2):
   return sorted_list    
       
 print(merge_lists([1,2,20], [4,6,7,8]))
+
+
+###############OR ###########
+
+def merge(a, b):
+  """
+  a and b are two sorted lists
+  """
+  i = 0
+  j = 0
+  c =  []
+  while i < len(a) and j < len(b):
+    if a[i] < b[j]:
+      c.append(a[i])
+      i += 1
+    else:
+      c.append(b[j])
+      j += 1
+  if i < len(a):
+    while i < len(a):
+      c.append(a[i])
+      i += 1
+  if j < len(b):
+    while j < len(b):
+      c.append(b[j])
+      j += 1
+  return c
+
+print(merge([1,2,20], [4,6,7,8]))
